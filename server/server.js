ce9c2bec74617ca3af694e3848939f47f00438ff
Joucks Hua <joucks@outlook.com>
2020-06-04 17:27:11 +0800

    up

--- server/server.js
+++ server/server.js
@@ -198,7 +198,7 @@ const server = net.createServer(function (client_sock) {
                     let my = room.clientArr.find(s => s.ip == ipStr)
                     let vote_player = room.clientArr.find(s => s.num == obj.data.num)
                     if (vote_player) {
-                        vote_player.vote_num += 1
+                        my.vote_num += 1
                     }
                     my.is_vote = true
                     my.is_speak = false
